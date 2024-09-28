51bd8998d7f17e735d30b53e3b24df8e6d4cd690
Mayank Gupta <mayankg5758@gmail.com>
2024-09-28 21:51:17 +0530

    Add

--- Spiral_Matrix54.java
+++ Spiral_Matrix54.java
@@ -1,18 +1,40 @@
-public class Spiral_Matrix54 {
+import java.util.*;
+class Solution {
+    public List<Integer> spiralOrder(int[][] arr) {
+        int startCol = 0;
+        int startRow = 0;
+        int lastRow = arr.length - 1;
+        int lastCol = arr[0].length - 1;
+        List<Integer> list = new ArrayList<>();
 
-    static void hells(int[][] matrix) {
-        int left = 0;
-        int top = 0;
-        int right = matrix.length;
-        // int botttom = matrix[0].length;
+        while (startCol <= lastCol && startRow <= lastRow) {
+            for (int i = startCol; i <= lastCol; i++) {
+                list.add(arr[startRow][i]);
+            }
 
-        for (int i = left; i < right; i++) {
-            System.out.println(matrix[i][top]);
+            for (int i = startRow + 1; i <= lastRow; i++) {
+                list.add(arr[i][lastCol]);
+            }
+
+            for (int i = lastCol - 1; i >= startCol; i--) {
+                if (startRow == lastRow) {
+                    break;
+                }
+                list.add(arr[lastRow][i]);
+            }
+
+            for (int i = lastRow - 1; i >= startRow + 1; i--) {
+                if (startCol == lastCol) {
+                    break;
+                }
+                list.add(arr[i][startCol]);
+            }
+
+            startCol++;
+            startRow++;
+            lastCol--;
+            lastRow--;
         }
-    }
-    public static void main(String[] args) {
-        int[][] matrix = {{1,2,3}, {4,5,6}, {7,8,9}};
-        hells(matrix);
-
+        return list;
     }
 }
